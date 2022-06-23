e5b4b881bf1876eb9a9454816ce9a3033729136f
Shijun Park <shijun2009@naver.com>
2022-06-23 21:25:50 +0900

    header

---
+++ add_key.js
@@ -0,0 +1,7 @@
+const script = document.createElement("script");
+
+const fontAwesome = config.apikey;
+
+script.src = `https://kit.fontawesome.com/${fontAwesome}.js`;
+
+document.head.appendChild(script);
